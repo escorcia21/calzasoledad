dbba880f299544b3ff148b21e99bd94a539ad01b
carlos escorcia <carlsescorcia21@hotmail.com>
2023-10-08 08:41:17 -0500

    delete space

--- models/BaseModel.py
+++ models/BaseModel.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import DeclarativeBase
 from configs.Database import Engine
 
 class Base(DeclarativeBase):
-        pass
+    pass
 
 def init():
     Base.metadata.create_all(bind=Engine)
